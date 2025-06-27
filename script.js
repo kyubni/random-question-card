db62360cc963dd7c2f933c94dcf035e4b8783dae
kyubni <rkffpq0818@gmail.com>
2025-06-28 05:58:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,7 @@ const card = document.getElementById("card");
 const backImage = document.getElementById("back-image");
 const resetBtn = document.getElementById("reset-btn");
 
-const backImages = [
-  "images/back1.jpg",
-  "images/back2.jpg",
-  "images/back3.jpg",
-  "images/back4.jpg"
-];
+const backImages = Array.from({ length: 8 }, (_, i) => `images/back${i + 1}.jpg`);
 
 let isFlipped = false;
 
